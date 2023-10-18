feae70eaa87f148f6adf86bce54ab56e138abfee
gellidinesh24 <139426071+gellidinesh24@users.noreply.github.com>
2023-10-18 14:03:11 +0530

    Add files via upload

---
+++ sqrt.py
@@ -0,0 +1,8 @@
+num = 6
+# change the value of num for different results
+# num = float(input('enter a number:'))
+num_sqrt = num**0.5
+print("the square root of num is \n",num_sqrt)
+# num = int(input('enter a number:'))
+num_sq = num**4
+print("the square  of num is \n",num_sq)
