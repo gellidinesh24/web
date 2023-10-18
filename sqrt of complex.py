9463e9d68857db5c8408636714c585a84057a26e
gellidinesh24 <139426071+gellidinesh24@users.noreply.github.com>
2023-10-18 14:51:25 +0530

    Add files via upload

---
+++ sqrt of complex.py
@@ -0,0 +1,9 @@
+import cmath
+# by using integer complex numbers
+num = 2 + 6j
+num_sqrt = cmath.sqrt(num)
+print('the square root of complex number is', num_sqrt)
+# by using float complex numbers
+num1 = 1.75 + 5.2j
+num_sqrt = cmath.sqrt(num1)
+print('the square root of complex number', num_sqrt)
