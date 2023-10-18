593d73d4683901e84052ba5e35ab5d79660d5828
gellidinesh24 <139426071+gellidinesh24@users.noreply.github.com>
2023-10-18 14:02:44 +0530

    Add files via upload

---
+++ logical oper.py
@@ -0,0 +1,10 @@
+num1 = 24
+num2 = 15
+sum = num1+num2
+difference = num1-num2
+product = num1*num2
+division = num1/num2
+print("the sum num1 and num2 is", sum)
+print("the difference of num1 and num2 is", difference)
+print("the product of num1 and num2 is", product)
+print("the division of num1 and num2 is", division)
