d0743ddbd6e72268f98101544ad467afb84e955a
gellidinesh24 <139426071+gellidinesh24@users.noreply.github.com>
2023-10-19 12:43:16 +0530

    Add files via upload

---
+++ check num png.py
@@ -0,0 +1,9 @@
+# program to check whether the number is positive,negative or zero
+num = int(input('enter the value'))
+# using if statements
+if num > 0:
+  print('positive')
+elif num==0 :
+   print('zero')
+else :
+   print('negative')
