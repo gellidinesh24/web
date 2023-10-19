bc3f3fe79f3c14af1e2267c90bfcce1e72b22402
gellidinesh24 <139426071+gellidinesh24@users.noreply.github.com>
2023-10-19 11:58:41 +0530

    Add files via upload

---
+++ swap 2 var.py
@@ -0,0 +1,16 @@
+# program to swap two variables
+# let x and y are two variables with their from user
+x = 4
+y = 7
+# to swap two variables create a temporary variable
+temp = x
+x = y
+y = temp
+print('the value of x after swap', format(x))
+print('the value of y after swap', format(y))
+
+
+
+
+
+
